b9dd33c30a21c94cacf5aa6a0fca7ee3714188f3
prachi gaware <139032263+prachigaware@users.noreply.github.com>
2023-08-14 01:04:36 -0700

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char s1[100][100],t[100];
+	int i,n,pass;
+	printf("enter limit");
+	scanf("%d",&n);
+	printf("enter n names of students");
+	for(i=0;i<n;i++)
+	{
+		scanf("%s",&s1[i]);
+	}
+	for(pass=1;pass<n;pass++)
+	{
+		for(i=0;i<n-pass;i++)
+		 {
+		 	if(strcmp(s1[i],s1[i+1])>0)
+		 	{
+		 		strcpy(t,s1[i]);
+		 		strcpy(s1[i],s1[i+1]);
+		 		strcpy(s1[i+1],t);
+			 }
+		 }
+	}
+	 printf("sorted array names");
+	 for(i=0;i<n;i++)
+	  {
+	  	 printf("%s\t",s1[i]);
+	  }
+}
